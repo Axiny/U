dc1664e587892c64e823d10f669e02efa7b193e6
Axiny <mr.axiny@gmail.com>
2022-08-17 17:30:02 +0900

    [feat]: add FileLoader Object

--- dist/U.js
+++ dist/U.js
@@ -320,11 +320,70 @@ class Fetch {
 
 }
 
+class FileLoader {
+
+    #options = {
+
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+
+    }
+
+    #feth = null;
+
+    constructor () {
+
+        this.#feth = new Fetch();
+
+    }
+
+    setOptions ( options ) {
+
+        this.#options = Object.assign(this.#options, options);
+
+    }
+
+    async stringBlob ( url, options ) {
+
+        const opts = Object.assign(this.#options, options);
+
+        const response = await this.#feth.get( url, opts );
+        const str = (await response.text()).toString();
+
+        return str;
+
+    }
+
+    async xml ( url, options ) {
+
+        const str = await this.stringBlob(url, options);
+        const xml = await new DOMParser().parseFromString(str, "text/xml");
+
+        return xml
+
+    }
+
+    async json ( url, options ) {
+
+        const str = await this.stringBlob (url, options);
+        const json = JSON.parse(str);
+
+        return json;
+
+    }
+
+}
+
 var index = {
 
     Fetch,
+    FileLoader,
     Type
 
 };
 
-export { Fetch, Type, index as default };
+export { Fetch, FileLoader, Type, index as default };
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import Type from "./Type"
 import Fetch from "./Fetch"
+import FileLoader from "./FileLoader"
 
 export default {
 
     Fetch,
+    FileLoader,
     Type
 
 }
 export {
 
     Fetch,
+    FileLoader,
     Type
     
 }
